a7a8c18faa03e7e6c9cb8f1ca924c814882f061f
João Paulo <joao.pauloll@hotmail.com>
2023-08-11 16:45:49 -0300

    H2 database, test profile, JPA

--- src/main/java/com/jp/springbootH2/entities/User.java
+++ src/main/java/com/jp/springbootH2/entities/User.java
@@ -3,10 +3,20 @@ package com.jp.springbootH2.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;

07ab2363d6a91f223f1d7bd0c9eadd53b0505ec3
João Paulo <joao.pauloll@hotmail.com>
2023-08-11 13:51:34 -0300

    Criação do projeto

---
+++ src/main/java/com/jp/springbootH2/SpringbootH2Application.java
@@ -0,0 +1,13 @@
+package com.jp.springbootH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jp/springbootH2/SpringbootH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.springbootH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

52348f1da356f5bfd33bf0e09e353ae223d4969d
João Paulo <joao.pauloll@hotmail.com>
2023-08-12 15:20:41 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/jp/springbootH2/config/TestConfig.java
+++ src/main/java/com/jp/springbootH2/config/TestConfig.java
@@ -58,5 +58,14 @@ public class TestConfig implements CommandLineRunner {
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 	}
 }
